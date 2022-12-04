760c3c699edb4f04a8083c783c02ee9cd2ef6d59
Dan Orzechowski <orzechod@gmail.com>
2022-12-03 21:24:57 -0500

    clone emacs

--- src/build-emacs.sh
+++ src/build-emacs.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export LD_LIBRARY_PATH=/usr/local/lib/tree-sitter && \
+    git clone --depth 1 --branch emacs-29 git://git.savannah.gnu.org/emacs.git && \
     cd emacs && \
     ./autogen.sh && \
     ./configure && \
