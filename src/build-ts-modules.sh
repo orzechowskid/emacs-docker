1f595eb94eccd79818966e34a5b77d0b0a75dfd4
Dan Orzechowski <orzechod@gmail.com>
2022-12-03 20:51:10 -0500

    node 16.x

--- src/build-ts-modules.sh
+++ src/build-ts-modules.sh
@@ -2,6 +2,13 @@
 
 git clone --depth 1 https://github.com/casouri/tree-sitter-module && \
     cd tree-sitter-module && \
-    ./batch.sh && \
-    mkdir -p /usr/local/lib/tree-sitter && \
+    ./batch.sh
+
+# build.sh tries to run `git clone` which will fail since this directory already
+# exists; this is expected since css-in-js does not live in the same git repo as
+# other languages built by `tree-sitter-module`
+git clone --depth 1 https://github.com/orzechowskid/tree-sitter-css-in-js && \
+    ./build.sh css-in-js
+
+mkdir -p /usr/local/lib/tree-sitter && \
     mv ./dist/* /usr/local/lib/tree-sitter/
